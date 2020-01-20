f457bc03032e7898e2692cf9a254e3c1a9622f59
Kurnia <kurniasuryai@gmail.com>
2020-01-21 00:12:44 +0100

    Created User class with constructors

---
+++ src/com/company/User.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Person {
+    private String userName;
+    private int id;
+
+    public Person(String userName, int id) {
+        this.userName = userName;
+        this.id = id;
+    }
+}
